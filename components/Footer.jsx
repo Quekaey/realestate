9be505197c17f6b9d504d0bcef98b70fed5fb266
U. Felix <68272832+Quekaey@users.noreply.github.com>
2022-06-06 00:10:14 +0100

    commit to master

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/layout';
 const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
     © 2022 Stallion Properties, Inc. 
-    <a href ="mailto:stallionproperties@inbox.ru" className="p-text">  Send Email to : stallionproperties@inbox.ru</a>
+    <br/> <a href ="mailto:stallionproperties@inbox.ru" className="p-text">Send Email: stallionproperties@inbox.ru</a>
   </Box>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -56,8 +56,8 @@ export default function Home ({ propertiesForSale, propertiesForRent }) {
 }
 
 export async function getStaticProps() {
-  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=10`);
-  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=10`);
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=12`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=12`);
 
   return {
     props: {
