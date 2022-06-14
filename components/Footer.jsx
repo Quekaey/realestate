576de4965573c4fd05cdbb61984e4e8c3dd8b3f6
U. Felix <68272832+Quekaey@users.noreply.github.com>
2022-06-14 22:55:30 +0100

    commit to master

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
     © 2022 Stallion Properties, Inc. 
     <br/> <a href ="mailto:stallionproperties@inbox.ru" className="p-text">Send Email: stallionproperties@inbox.ru</a>
+    <br/> <a href ="" className="p-text">Stallion Properties Suite 3104, Al-Manara Tower, Business Bay, Dubai, U.A.E.</a>
   </Box>
 );
 
