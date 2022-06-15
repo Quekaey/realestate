3b6a3bf1d3da49bed75b0b9b0bf015800cfbfa0d
U. Felix <68272832+Quekaey@users.noreply.github.com>
2022-06-15 09:59:36 +0100

    commit on master

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -7,7 +7,7 @@ export const fetchApi = async (url) => {
   const {data} = await axios.get((url), {
     headers: {
       'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-      'X-RapidAPI-Key': '6dcbc1b1bamsh0dd00e4fe6d72d4p1908dcjsn737b7a7ee441'
+      'X-RapidAPI-Key': 'cb33a5cd4amsha1b7455dafd6d82p1b13e2jsn2dfeb93239b5'
     }
   });
 
